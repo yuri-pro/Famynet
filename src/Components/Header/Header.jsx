ad7ffc16619fa7d2e162a5db87f870276e473e3e
yuri-pro <j.b060590@gmail.com>
2020-02-15 13:50:38 +0000

    creating header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import head from "./Header.module.css";
+
+const Header = ()=>{
+    return (
+        <header>
+                <h1>Famynet</h1>
+                <button>Log In</button>
+        </header>
+    )
+}
+
+export default Header;
