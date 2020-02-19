5330c9cebab1fa9ea48a41ede73778d985d671e5
yuri-pro <j.b060590@gmail.com>
2020-02-19 11:26:41 +0000

    creating msg section

--- src/Components/Messages/Messages.jsx
+++ src/Components/Messages/Messages.jsx
@@ -1,9 +1,23 @@
 import React from "react";
-import messages from "./Messages.module.css";
+import msg from "./Messages.module.css";
 
 const Messages = () =>{
     return(
-            <div>Messages</div>
+            <div className={msg.inner}>
+                <div className={msg.friendInner}>
+                    <div>Yuri</div>
+                    <div>Dmitrij</div>
+                    <div>Anastasija</div>
+                    <div>Evgenij</div>
+                    <div>Krowka</div>
+                    <div>Unknown</div>
+                </div>
+                <div className ={msg.chatInner}>
+                    <div>Hi how are you?</div>
+                    <div>Are you here?</div>
+                    <div>Yep, how can I help you?</div>
+                </div>
+            </div>
     )
 }
 
